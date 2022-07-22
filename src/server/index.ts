a2c569bb68e476ffd4090484cb5b58ecd97f05f9
Christian Tweed <christiantweed@gmail.com>
2022-07-22 17:33:47 -0400

    :hammer: Added build and dev scripts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,15 @@
-export const x: number = 4;
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Getting started');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️ Server is running at http://localhost:${port} ⚡️`);
+});

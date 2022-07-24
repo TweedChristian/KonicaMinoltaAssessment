74128ebacdd6a6c83f75d17f8839f774f60604fb
Christian Tweed <christiantweed@gmail.com>
2022-07-24 10:51:05 -0400

    :truck: Renamed the index.ts file to server.ts

--- src/server/routes/error.ts
+++ src/server/routes/error.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const errorRouter = express.Router();
 
-errorRouter.use("/", (req: Request, res: Response) => {
+errorRouter.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.sendStatus(200);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import initializeRouter from "./routes/initialize";
 import errorRouter from "./routes/error";
@@ -9,6 +10,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use((req: Request, _res: Response, next: NextFunction) => {
   console.log(`${req.method}: ${req.url}`);
   next();
